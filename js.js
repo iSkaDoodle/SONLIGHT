0588fccab0a32d8af7de2d013f0285330841acf2
iSkaDoodle <xtremenessgaming@gmail.com>
2025-04-14 14:20:56 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,14 @@
-window.addEventListener('scroll', () => {
-    const footer = document.getElementById('footer');
-    const scrollY = window.scrollY;
-    const viewportHeight = window.innerHeight;
-    const fullHeight = document.body.scrollHeight;
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('scroll', () => {
+      const footer = document.getElementById('footer');
+      const scrollY = window.scrollY;
+      const viewportHeight = window.innerHeight;
+      const fullHeight = document.body.scrollHeight;
   
-    if (scrollY + viewportHeight >= fullHeight - 10) {
-      footer.classList.add('visible');
-    } else {
-      footer.classList.remove('visible');
-    }
+      if (scrollY + viewportHeight >= fullHeight - 10) {
+        footer.classList.add('visible');
+      } else {
+        footer.classList.remove('visible');
+      }
+    });
   });
